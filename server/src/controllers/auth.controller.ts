241f864e43a53fda5f746e10cbf3a6beda3b9a1b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-17 20:56:48 -0300

    Add custom request type

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from 'express';
 import User from '../models/User.model';
 import { generateJWT } from '../helpers';
+import { IAuthRequest } from '../types';
 
 export const createUser = async (req: Request, res: Response) => {
 
@@ -11,7 +12,7 @@ export const createUser = async (req: Request, res: Response) => {
 
     if (user) return res.status(400).json({
       ok: false,
-      msg: 'Email is already in use',
+      msg: 'Email is already in use.',
     });
 
     user = new User(req.body);
@@ -45,14 +46,14 @@ export const singIn = async (req: Request, res: Response) => {
 
     if (!user) return res.status(400).json({
       ok: false,
-      msg: 'Email or Password incorrect',
+      msg: 'Email or password incorrect.',
     });
 
     const isValidPassword = user.comparePassword(password);
 
     if (!isValidPassword) return res.status(400).json({
       ok: false,
-      msg: 'Email or Password incorrect'
+      msg: 'Email or password incorrect.'
     });
 
     const token = await generateJWT({ uid: user.id, name: user.name, role: user.role  });
@@ -74,11 +75,12 @@ export const singIn = async (req: Request, res: Response) => {
   }
 };
 
-export const renewUser = async (req: Request, res: Response) => {
-
-  const { uid, name, role } = req.auth;
+export const renewUser = async (req: IAuthRequest, res: Response) => {
 
   try {
+    if (!req.auth) throw new Error;
+    const { uid, name, role } = req.auth;
+
     const token = await generateJWT({ uid, name, role });
 
     return res.json({
@@ -95,6 +97,4 @@ export const renewUser = async (req: Request, res: Response) => {
       msg: 'Internal server error.'
     });
   }
-
-
 };
