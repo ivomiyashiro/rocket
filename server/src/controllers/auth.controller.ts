13b4d36d6e76eb4add95b7145933d2df2ac48c06
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-18 10:14:31 -0300

    Fix reponse structure and Add google signin

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Response, Request } from 'express';
 import User from '../models/User.model';
-import { generateJWT } from '../helpers';
 import { IAuthRequest } from '../interfaces';
+import { generateJWT } from '../helpers';
+import { getGoogleOAuthTokens, getGoogleUser } from '../services';
 
-export const createUser = async (req: Request, res: Response) => {
+export const signUp = async (req: Request, res: Response) => {
 
   const { email } = req.body;
 
@@ -12,19 +13,23 @@ export const createUser = async (req: Request, res: Response) => {
 
     if (user) return res.status(400).json({
       ok: false,
-      msg: 'Email is already in use.',
+      msg: 'Email is already in use.'
     });
 
     user = new User(req.body);
     await user.save();
 
     const token = await generateJWT({ uid: user.id, name: user.name, role: user.role  });
+    res.cookie('token', token);
 
     return res.status(201).json({
       ok: true,
-      uid: user.id,
-      name: user.name,
-      token
+      user: {
+        uid: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role
+      }
     });
 
   } catch (error) {
@@ -57,12 +62,16 @@ export const singIn = async (req: Request, res: Response) => {
     });
 
     const token = await generateJWT({ uid: user.id, name: user.name, role: user.role  });
+    res.cookie('token', token);
 
     return res.status(200).json({
       ok: true,
-      uid: user.id,
-      name: user.name,
-      token
+      user: {
+        uid: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role
+      }
     });
 
   } catch (error) {
@@ -82,13 +91,66 @@ export const renewUser = async (req: IAuthRequest, res: Response) => {
     const { uid, name, role } = req.auth;
 
     const token = await generateJWT({ uid, name, role });
+    res.cookie('token', token);
+
+    return res.json({ ok: true });
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      msg: 'Internal server error.'
+    });
+  }
+};
+
+export const googleOauthHandler = async (req: Request, res: Response) => {
+
+  const code = req.query.code as string;
+
+  try {
+    const { id_token, access_token } = await getGoogleOAuthTokens({ code });
+    console.log({ id_token, access_token });
+
+    const googleUser = await getGoogleUser({ id_token, access_token });
+
+    console.log({ googleUser });
+
+    if (!googleUser.verified_email) {
+      return res.status(403).json({
+        ok: false,
+        msg: 'Google account is not verified.'
+      });
+    }
+
+    // upsert the user
+    const user = await User.findOneAndUpdate(
+      { email: googleUser.email },
+      {
+        email: googleUser.email,
+        name: googleUser.name
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    );
+
+    const token = await generateJWT({ uid: user.id, name: user.name, role: user.role });
+
+    res.cookie('accessToken', token);
 
     return res.json({
       ok: true,
-      uid,
-      name,
-      token
+      user: {
+        uid: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role
+      }
     });
+
   } catch (error) {
     console.log(error);
 
