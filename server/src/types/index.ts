8b3cc8ecef482663905cb143f2ba77196e981f2b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-17 21:00:58 -0300

    Change file name

--- server/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-
-export interface IAuthRequest extends Request {
-  auth?: {
-    uid: string,
-    name: string,
-    role: 'CUSTOMER' | 'ADMIN' | 'SUPERADMIN',
-  }
-}
