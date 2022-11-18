0cc553d4332f9702ed823c860016ce2a75b90a4a
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-18 11:21:44 -0300

    Add random password generator

--- server/src/helpers/genJWT.helper.ts
+++ server/src/helpers/genJWT.helper.ts
@@ -9,7 +9,7 @@ interface IProps {
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRES = process.env.JWT_EXPIRES as string;
 
-export const generateJWT = ({ uid, name, role }: IProps) => {
+export const genJWT = ({ uid, name, role }: IProps) => {
 
   return new Promise((resolve, reject) => {
 
