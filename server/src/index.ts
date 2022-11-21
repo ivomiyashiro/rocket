cf885ffc5ee6d9bc1890492f9a2449caee7c9918
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-20 22:44:46 -0300

    Add cookie parser config and productsRouter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { config } from 'dotenv'; config();
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-import { AuthRouter } from './routes';
+import { AuthRouter, ProductsRouter } from './routes';
 import { dbConnection } from './database/config';
 
 const app = express();
@@ -13,9 +14,11 @@ dbConnection();
 // Config
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', AuthRouter);
+app.use('/api/products', ProductsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running in port ${ process.env.PORT }`);
