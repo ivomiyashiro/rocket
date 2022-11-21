c3fcac9ca8486cdf7b4102da8c94722c0dba60b4
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-21 13:01:19 -0300

    Change variant scheme export

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,3 @@
 export { default as User } from './User.model';
 export { default as Product } from './Product.model';
-export { default as Variant } from './Variant.model';
+export { VariantScheme } from './Variant.model';
