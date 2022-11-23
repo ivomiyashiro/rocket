5b35ac827da74f1ab19f7a3ba689b91e0f643a5d
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-22 22:57:07 -0300

    Add order model

---
+++ server/src/models/Order.model.ts
@@ -0,0 +1,59 @@
+import { Schema, model } from 'mongoose';
+import { IOrder } from '../interfaces';
+
+const OrderScheme = new Schema<IOrder>({
+  productsIDs: {
+    type: [String],
+    requied: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['DELIVERED', 'PENDING', 'CANCELLED'],
+    default: 'PENDING'
+  },
+  customer: {
+    type: {
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      }
+    },
+    required: true
+  },
+  shippingAddress: {
+    type: {
+      city: {
+        type: String,
+        required: true
+      },
+      locality: {
+        type: String,
+        require: true,
+      },
+      address: {
+        type: String,
+        require: true,
+      },
+      zip: {
+        type: String,
+        require: true,
+      }
+    },
+    required: true
+  }
+}, { 
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+export default model<IOrder>('Order', OrderScheme);
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as User } from './User.model';
 export { default as Product } from './Product.model';
 export { VariantScheme } from './Variant.model';
+export { default as Order } from './Order.model';
