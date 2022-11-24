d9d95894758cc71ac351914018afa3a216346058
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-24 17:05:50 -0300

    Add tailwind

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'signin-window': '400px'
+      },
+      maxHeight: {
+        'signin-window': '500px'
+      }
+    },
   },
   plugins: [],
 }
