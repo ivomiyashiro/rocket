29039f7e26bac3801a1b150366b347bea1f56169
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-24 08:54:55 -0300

    Add product searcher

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -3,7 +3,7 @@ import { Product } from '../models';
 
 const DEFAULT_LIMIT = 10;
 const DEFAULT_PAGE = 1;
-const DEFAULT_FILTERS = '{}';
+const DEFAULT_FILTERS = {};
 const DEFAULT_SORT_BY = 'title';
 const DEFAULT_ORDER_BY = 'asc';
 
@@ -11,14 +11,14 @@ export const getAllProducts = async (req: Request, res: Response) => {
 
   const { sortBy: reqSortBy, orderBy: reqOrderBy, limit: reqLimit, page: reqPage, filters: reqFilters }: any = req.query;
 
-  const filters = reqFilters || DEFAULT_FILTERS;
+  const filters = JSON.parse(reqFilters) || DEFAULT_FILTERS;
   const orderBy = reqOrderBy || DEFAULT_ORDER_BY;
   const sortBy = reqSortBy || DEFAULT_SORT_BY;
   const limit = Number(reqLimit) || DEFAULT_LIMIT;
   const page = Number(reqPage) || DEFAULT_PAGE;
 
   try {
-    const products = await Product.find( JSON.parse(filters || DEFAULT_FILTERS) )
+    const products = await Product.find(filters)
       .limit(limit)
       .skip((page - 1) * limit)
       .sort([[sortBy, orderBy]])
@@ -129,6 +129,42 @@ export const updateProduct = async (req: Request, res: Response) => {
   }
 };
 
-// export const searchProduct = (req: Request, res: Response) => {
-  
-// };
+export const searchProducts = async (req: Request, res: Response) => {
+
+  const { sortBy: reqSortBy, orderBy: reqOrderBy, limit: reqLimit, page: reqPage }: any = req.query;
+
+  const search = req.params.search || '';
+  const orderBy = reqOrderBy || DEFAULT_ORDER_BY;
+  const sortBy = reqSortBy || DEFAULT_SORT_BY;
+  const limit = Number(reqLimit) || DEFAULT_LIMIT;
+  const page = Number(reqPage) || DEFAULT_PAGE;
+
+  try {
+    const products = await Product.find({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { category: { $regex: search, $options: 'i' } },
+        { vendor: { $regex: search, $options: 'i' } },
+      ]
+    }).limit(limit)
+      .skip((page - 1) * limit)
+      .sort([[sortBy, orderBy]])
+      .exec();
+
+    const count = await Product.count();
+
+    return res.json({
+      ok: true,
+      products,
+      totalPages: Math.ceil(count / limit)
+    });
+    
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      msg: 'Internal server error.'
+    });
+  }
+};
