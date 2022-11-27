adaccb1ece5d6341c43d616e3ade86545cb179d0
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 16:47:37 -0300

    Fix request header and remove body

--- ui/services/checkDBToken.service.ts
+++ ui/services/checkDBToken.service.ts
@@ -4,9 +4,12 @@ import axios, { AxiosError } from 'axios';
 export const checkDBToken = async ({ token }: { token: string }) => {
   try {
     const { data } = await axios.post(
-      process.env.NEXT_PUBLIC_SERVER_ENDPOINT as string + '/auth/renew', {
-        token
-      }, { withCredentials: true }
+      process.env.NEXT_PUBLIC_SERVER_ENDPOINT as string + '/auth/renew', {}, { 
+        withCredentials: true,
+        headers: {
+          Cookie: `token=${ token }`
+        }
+      }
     );
 
     return data;
