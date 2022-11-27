0ccf8711f52b004d19f5426c2b746f473dd16391
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 18:34:31 -0300

    Change file names

---
+++ ui/components/pages/Signup/components/CredentialsSignUpForm.tsx
@@ -0,0 +1,62 @@
+import { Button, Input } from 'components/ui';
+import { useSignUpCredentials } from '../hooks';
+
+export const CredentialsSignInForm = () => {
+
+  const {
+    fullNameInput,
+    emailInput, 
+    passwordInput, 
+    isLoading,
+    setFullNameInput,
+    setEmailInput, 
+    setPasswordInput, 
+    handleSubmit 
+  } = useSignUpCredentials();
+
+  return (
+    <form className='my-5' onSubmit={ handleSubmit }> 
+      <div>
+        <Input 
+          type='text'
+          placeholder='Enter your full name...'
+          label='Full Name'
+          name='name'
+          id='name'
+          inputValue={ fullNameInput.value }
+          error={ fullNameInput.error }
+          handleInputValue={ setFullNameInput }
+        />
+        <Input 
+          type='email'
+          placeholder='Enter your email...'
+          label='Email'
+          name='email'
+          id='email'
+          inputValue={ emailInput.value }
+          error={ emailInput.error }
+          handleInputValue={ setEmailInput }
+        />
+        <Input 
+          type='password'
+          placeholder='Enter your password...'
+          label='Password'
+          name='password'
+          id='password'
+          inputValue={ passwordInput.value }
+          error={ passwordInput.error }
+          handleInputValue={ setPasswordInput }
+        />
+        <div className='mt-6'>
+          <Button type='button' bgColor="bg-indigo-600" textColor="text-white">
+            {
+              isLoading
+                ? 'Cargando...'
+                : 'Sign up'
+            }
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
