bdbeacab533aad273d5a56ce71c88f865d649564
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 14:43:55 -0300

    Fix middlewares validations

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -3,7 +3,7 @@ import { check } from 'express-validator';
 
 import { validateFields, validateJWT } from '../middlewares';
 
-import { signUp, singIn, renewUser, googleOauthHandler } from '../controllers/auth.controller';
+import { signUp, singIn, renewToken, googleOauthHandler } from '../controllers/auth.controller';
  
 const router = Router();
  
@@ -11,9 +11,10 @@ router.post(
   '/signup',
   [
     check('name', 'Name is required.').not().isEmpty(),
-    check('email', 'Email is required.').isEmail(),
+    check('email', 'Email is required.').not().isEmpty(),
+    check('email', 'Email is not valid.').isEmail(),
     check('password', 'Password is required.').not().isEmpty(),
-    check('password', 'Password is required.').isLength({ min: 6 }),
+    check('password', 'Password length must be higher than 6.').isLength({ min: 6 }),
     validateFields
   ],
   signUp
@@ -22,14 +23,15 @@ router.post(
 router.post(
   '/signin',
   [
+    check('email', 'Email is required.').not().isEmpty(),
     check('email', 'Email is required.').isEmail(),
-    check('password', 'Password is required.').isLength({ min: 6 }),
+    check('password', 'Password is required.').not().isEmpty(),
     validateFields
   ],
   singIn
 );
  
-router.get('/renew', validateJWT, renewUser);
+router.post('/renew', validateJWT, renewToken);
 
 router.get('/signin/google', googleOauthHandler);
  
