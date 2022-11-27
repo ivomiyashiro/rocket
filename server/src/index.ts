f42475fb1208b953f02650330cd1834e1ab53004
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 14:43:24 -0300

    Fix cors options to enable cookies response

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,11 @@ const app = express();
 dbConnection();
 
 // Config
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: [ 'http://localhost:3000', 'http://127.0.0.1', 'http://104.142.122.231' ],
+  credentials: true,
+  exposedHeaders: ['set-cookie'],
+}));
 app.use(express.json());
 app.use(cookieParser());
 
