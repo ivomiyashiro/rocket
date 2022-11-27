105a35b855ba1e8352f86e6671b0d78e215422b9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 18:53:11 -0300

    Update tailwind config

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       maxWidth: {
