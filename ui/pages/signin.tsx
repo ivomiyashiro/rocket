941adba60ec4e3969e09c265059ddc291c063ab0
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-27 16:48:47 -0300

    Add route protection for logged users

--- ui/pages/signin.tsx
+++ ui/pages/signin.tsx
@@ -1,11 +1,40 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 import { SigninPage as Page } from 'components/pages';
+import { checkDBToken } from 'services';
 
 const SigninPage = () => {
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500'>
-      <Page />
-    </div>
+    <>
+      <Head>  
+        <title>Signin - Rocket</title>
+        <meta name="description" content="Signin to Rocket." />
+      </Head>
+      <div className='w-full h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500'>
+        <Page />
+      </div>
+    </>
   );
 };
 
 export default SigninPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+  const token = req.cookies.token || '';
+  const { user } = await checkDBToken({ token });
+
+  const { p = '/' } = query;
+
+  if (user) {
+    return {
+      redirect: {
+        destination: p.toString(),
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: { }
+  };
+};
