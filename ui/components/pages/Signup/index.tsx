7cbda75ff3ff6bced8d67042658d42426265beec
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-28 20:13:38 -0300

    Fix logo link

--- ui/components/pages/Signup/index.tsx
+++ ui/components/pages/Signup/index.tsx
@@ -7,12 +7,12 @@ export const SignupPage = () => {
   return (
     <div className='flex bg-white shadow-2xl h-full w-full sm:max-h-signup-window sm:max-w-signup-window sm:rounded-2xl'>
       <div className='px-6 py-12 w-full'>
-        <Link href='/'>
-          <div className='inline-flex items-center gap-2'>
+        <div className='inline-flex items-center gap-2'>
+          <Link href='/'>
             <RocketIcon size='32' color='black' />
-            <p className='text-3xl font-bold'>Sign up</p>
-          </div>
-        </Link>
+          </Link>
+          <p className='text-3xl font-bold'>Sign up</p>
+        </div>
         <CredentialsSignInForm />
         <div className='relative w-full text-center flex items-center justify-center my-10'>
           <div className='w-full h-px bg-gray-300'/>
