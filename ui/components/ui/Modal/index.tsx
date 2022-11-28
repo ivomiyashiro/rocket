d707c77f7818b863f2e333e989b66c3feee3a65f
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-28 20:12:54 -0300

    Add Modal

---
+++ ui/components/ui/Modal/index.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, ReactNode, SetStateAction } from 'react';
+
+interface Props {
+  children: ReactNode;
+  isOpen: boolean;
+  handleOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Modal = ({ children, isOpen, handleOpen }: Props) => {
+  return (
+    <>
+      <div
+        className={ `h-screen fixed top-0 z-10 left-0 ${ isOpen ? 'w-full' : 'w-0'}` }
+        onClick={ () => handleOpen(false) }
+      />
+      { children }
+    </>
+  );
+};
