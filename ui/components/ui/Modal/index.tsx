2fd7639d19862393890699d5de363b7079b902d3
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-30 12:07:39 -0300

    Add optional background to modal

--- ui/components/ui/Modal/index.tsx
+++ ui/components/ui/Modal/index.tsx
@@ -5,14 +5,15 @@ interface Props {
   isOpen: boolean;
   align: 'start' | 'center' | 'end';
   justify: 'start' | 'center' | 'end';
+  withBackground?: boolean;
   handleOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export const Modal = ({ children, isOpen, align, justify, handleOpen }: Props) => {
+export const Modal = ({ children, isOpen, align, justify, withBackground = false, handleOpen }: Props) => {
   return (
     <div className={ `fixed top-0 left-0 flex items-${ align } justify-${ justify } z-20 ${ isOpen ? 'w-full' : 'w-0'}` }>
       { children }
-      <div className={ `h-full w-full fixed z-10 ${ isOpen ? 'block' : 'hidden'}` } onClick={ () => handleOpen(false) } />
+      <div className={ `h-full w-full fixed z-10 ${ isOpen ? 'block' : 'hidden'} ${ withBackground && 'bg-black bg-opacity-50'}` } onClick={ () => handleOpen(false) } />
     </div>
   );
 };
