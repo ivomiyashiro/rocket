d04f651f41f6ad9c390f71f547facdc2a2ee1fbf
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-12-02 08:54:32 -0300

    Change envs from public to private

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_OAUTH_REDIRECT_URL: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+    CLAUDINARY_URL: process.env.CLAUDINARY_URL
+  },
   images: {
     domains: ['res.cloudinary.com', 'pbs.twimg.com'],
   },
