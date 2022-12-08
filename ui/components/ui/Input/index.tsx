b075dc09b85915b9eee55b460e91c808c3637842
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-12-08 09:22:50 -0300

    Add optional spinner when input is number

--- ui/components/ui/Input/index.tsx
+++ ui/components/ui/Input/index.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FocusEvent, KeyboardEvent, KeyboardEventHandler, ReactElement } from 'react';
+import { ChangeEvent, FocusEvent, KeyboardEvent, ReactElement } from 'react';
+import { TriangeUpIcon, TriangleDownIcon } from 'components/icons';
 
 interface Props {
   inputValue: string;
@@ -8,22 +9,23 @@ interface Props {
   name?: string;
   id?: string;
   textField?: string | ReactElement;
+  withError?: boolean;
+  withSpinner?: boolean;
   error: string;
   onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleIncreaseQuantity?: () => void;
+  handleDecreaseQuantity?: () => void;
 }
 
 export const Input = ({ 
-  inputValue, 
-  placeholder,
-  type,
-  label = '',
-  name = '',
-  id = '',
-  textField,
-  error,
-  onBlur,
-  onChange,
+  inputValue, placeholder,
+  type, label = '',
+  name = '', id = '',
+  withError = true, withSpinner = false,
+  textField, error,
+  onBlur, onChange,
+  handleIncreaseQuantity, handleDecreaseQuantity
 }: Props) => {
 
   const INPUT_TYPE = type === 'number' ? 'text' : type;
@@ -36,21 +38,35 @@ export const Input = ({
   return (
     <div className='w-full'>
       { !!label && <label className={ `text-sm mb-1 inline-block ${ !!error ? 'text-red-500' : 'text-gray-600'}` } htmlFor={ id }>{ label }</label> } 
-      <div className={ `flex items-center border border-gray-200 bg-gray-100 rounded-md outline outline-transparent ${ !!error ?  'border-red-500 bg-red-100 text-red-500 placeholder-red-400' : 'focus-within:outline-indigo-600'}` }>
+      <div className={ `w-full flex items-center border border-gray-200 bg-gray-100 rounded-md outline outline-transparent ${ !!error ?  'border-red-500 bg-red-100 text-red-500 placeholder-red-400' : 'focus-within:outline-indigo-600'}` }>
         { !!textField && <div className='ml-2 text-gray-600'> { textField } </div> }
-        <input 
-          type={ INPUT_TYPE }
-          placeholder={ placeholder }
-          value={ inputValue }
-          name={ name }
-          id={ id }
-          onBlur={ onBlur }
-          onChange={ onChange }
-          onKeyPress={ handleInputNumberChange }
-          className='w-full py-2 px-3 rounded-md text-sm bg-transparent outline-none'
-        />
+        <div className='flex w-full'>
+          <input 
+            type={ INPUT_TYPE }
+            placeholder={ placeholder }
+            value={ inputValue }
+            name={ name }
+            id={ id }
+            onBlur={ onBlur }
+            onChange={ onChange }
+            onKeyPress={ handleInputNumberChange }
+            className='w-full py-2 px-3 rounded-md text-sm bg-transparent outline-none'
+          />
+          { 
+            (withSpinner && type === 'number' && handleDecreaseQuantity && handleIncreaseQuantity) 
+            && 
+            <div className='flex flex-col h-full'>
+              <button className='flex items-center justify-center bg-gray-200 m-px mr-2px rounded-md w-4 h-4' onClick={ handleIncreaseQuantity }>
+                <TriangeUpIcon size='12px' />
+              </button>
+              <button className='flex items-center justify-center bg-gray-200 m-px mr-2px rounded-md w-4 h-4' onClick={ handleDecreaseQuantity }>
+                <TriangleDownIcon size='12px' />
+              </button>
+            </div>
+          }
+        </div>
       </div>
-      <p className={ `opacity-0 ${ !!error && 'opacity-100' } text-xs text-red-500 mt-1` }>* { error }</p>
+      { withError && <p className={ `opacity-0 ${ !!error && 'opacity-100' } text-xs text-red-500 mt-1` }>* { error }</p> }
     </div>
   );
 };
