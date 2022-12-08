735e3c628db366234ad56e24b2f3f25be83fcfc4
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-12-08 09:25:45 -0300

    Fix z-index

--- ui/components/ui/Modal/index.tsx
+++ ui/components/ui/Modal/index.tsx
@@ -3,17 +3,17 @@ import { Dispatch, ReactNode, SetStateAction } from 'react';
 interface Props {
   children: ReactNode;
   isOpen: boolean;
-  align: 'start' | 'center' | 'end';
-  justify: 'start' | 'center' | 'end';
+  align?: 'start' | 'center' | 'end';
+  justify?: 'start' | 'center' | 'end';
   withBackground?: boolean;
   handleOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export const Modal = ({ children, isOpen, align, justify, withBackground = false, handleOpen }: Props) => {
+export const Modal = ({ children, isOpen, align = 'center', justify = 'center', withBackground = false, handleOpen }: Props) => {
   return (
-    <div className={ `fixed top-0 left-0 flex items-${ align } justify-${ justify } ${ isOpen ? 'w-full' : 'w-0'}` }>
+    <div className={ `fixed top-0 left-0 flex h-screen overflow-hidden z-30 items-${ align } justify-${ justify } ${ isOpen ? 'w-full' : 'w-0'}` }>
       <div className='relative z-30 top-0'>{ children }</div>
-      <div className={ `h-full w-full absolute z-20 ${ isOpen ? 'block' : 'hidden'} ${ withBackground && 'bg-black bg-opacity-50'}` } onClick={ () => handleOpen(false) } />
+      <div className={ `h-full w-full absolute ${ isOpen ? 'block' : 'hidden'} ${ withBackground && 'bg-black bg-opacity-50'}` } onClick={ () => handleOpen(false) } />
     </div>
   );
 };
