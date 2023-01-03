4ed8bdacf7bea5e7b54301fbd231cb278bb3eb5f
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-01-03 08:32:44 -0300

    Add custom color and size

--- ui/components/ui/MainLogo/index.tsx
+++ ui/components/ui/MainLogo/index.tsx
@@ -2,16 +2,31 @@ import { RocketIcon } from 'components/icons';
 import Link from 'next/link';
 
 interface Props { 
-  href: string;
+  href?: string;
+  textColor?: string;
+  iconColor?: string;
 }
 
-export const MainLogo = ({ href }: Props) => {
+export const MainLogo = ({ href, textColor = '#000', iconColor = '#000' }: Props) => {
   return (
-    <Link href={ href }>
-      <div className='text-indigo-600 flex items-center gap-1'>
-        <RocketIcon size='35px' />
-        <p className='text-2xl italic font-extrabold text-gray-700'>Rocket</p>
-      </div>
-    </Link>
+    <>
+      {
+        !!href
+          ? (
+            <Link href={ href }>
+              <div className='text-indigo-600 flex items-center gap-1'>
+                <RocketIcon size='30px' color={ iconColor } />
+                <p className='text-2xl italic font-extrabold' style={ { color: textColor } }>Rocket</p>
+              </div>
+            </Link>
+          )
+          : (
+            <div className='text-indigo-600 flex items-center gap-1'>
+              <RocketIcon size='30px' color={ iconColor } />
+              <p className='text-2xl italic font-extrabold' style={ { color: textColor } }>Rocket</p>
+            </div>
+          )
+      }
+    </>
   );
 };
