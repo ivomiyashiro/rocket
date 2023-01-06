d462102d80d88904c8d18ffa101a2980304995dc
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-01-06 11:13:38 -0300

    Add inventory and remove unnesesary logs

--- ui/helpers/formatProductFormDataToDB.helper.ts
+++ ui/helpers/formatProductFormDataToDB.helper.ts
@@ -6,11 +6,11 @@ interface Props { data: IProductForm }
 export const formatProductFormDataToDB = ({ data }: Props): IProduct => {
 
   if ( data.options.length !== 0) {
-    console.log(data.options.map(opt => ({ name: opt.name.value, values: opt.values.map(val => ( val.name )) })));
     return {
       title: data.title.value,
       vendor: data.vendor.value,
       category: data.category.value,
+      inventory: data.variants.map(variant => +variant.inventory).reduce((partialSum, a) => partialSum + a, 0).toString(),
       options: data.options.map(opt => ({ name: opt.name.value, values: opt.values.map(val => ( val.name )) })),
       variants: data.variants.map(variant => ({ ...variant, images: variant.images.map( img => img.url ) })) as any,
       description: data.description.value,
@@ -23,6 +23,7 @@ export const formatProductFormDataToDB = ({ data }: Props): IProduct => {
     title: data.title.value,
     vendor: data.vendor.value,
     category: data.category.value,
+    inventory: data.inventory,
     description: data.description.value,
     price: data.price || '0',
     discountPrice: data.discountPrice || '0',
