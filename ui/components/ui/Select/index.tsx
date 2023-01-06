dfa08da5543a69c4a66919a1fcb012ea63ad95ca
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-01-06 11:07:46 -0300

    Fix onChange

--- ui/components/ui/Select/index.tsx
+++ ui/components/ui/Select/index.tsx
@@ -1,14 +1,15 @@
 import { TriangleDownIcon } from 'components/icons';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, ReactNode, useState } from 'react';
 
 interface Props {
   id: string;
+  icon?: ReactNode;
   label?: string;
   options: string[];
-  onChange?: () => void;
+  onChange?: any;
 }
 
-export const Select = ({ id, label, options, onChange }: Props) => {
+export const Select = ({ id, label, options, icon: Icon, onChange }: Props) => {
 
   const [option, setOption] = useState(options[0]);
   const [isFocus, setFocus] = useState(false);
@@ -19,19 +20,22 @@ export const Select = ({ id, label, options, onChange }: Props) => {
   };
 
   return (
-    <div className={ `relative border border-gray-200 bg-gray-100 h-[36px] rounded-md ${ isFocus && 'outline outline-indigo-600' }` }>
+    <div className={ `relative border border-gray-200 bg-gray-100 h-full rounded-md ${ isFocus && 'outline outline-indigo-600' }` }>
       { !!label && <p>label</p> }
       <select 
         id={ id } 
-        onChange={ (e) => { handleSelectChange(e); !!onChange && onChange(); } } 
+        onChange={ (e) => { handleSelectChange(e); !!onChange && onChange(e); } } 
         onFocus={ () => setFocus(true) }
         onBlur={ () => setFocus(false) }
         className="absolute top-0 w-full h-full opacity-0"
       >
         { options.map((opt, i) => <option key={ i }>{ opt }</option>) }
       </select>
-      <div className='h-full flex items-center justify-between px-3 text-sm'>
-        <span>{ option }</span>
+      <div className='h-full flex items-center gap-3 justify-between px-3 text-sm overflow-hidden'>
+        <div className='flex gap-2 text-gray-600'>
+          { !!Icon && <div className='mr-1'> { Icon } </div> }
+          <span className='whitespace-nowrap'>{ option }</span>
+        </div>
         <TriangleDownIcon size='16px' />
       </div>
     </div>
