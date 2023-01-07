7b05d2b06f6647c5ecd7875877ace8b04d1a4794
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-01-07 18:31:11 -0300

    Add table skeleton and menu

--- ui/components/pages/DasboardProductList/components/index.ts
+++ ui/components/pages/DasboardProductList/components/index.ts
@@ -2,3 +2,5 @@ export * from './ProductsTableHeader';
 export * from './ProductsTable';
 export * from './Searchbar';
 export * from './NoProductsMessage';
+export * from './ProductsTableMenu';
+export * from './ProductsTableSkeleton';
